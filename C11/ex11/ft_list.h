3a51ec0701b09dc46e89892094d81cdb15c2af53
PaulJKathmann <paul.kathmann.98@gmail.com>
2023-03-16 16:33:21 +0100

    too easy

---
+++ C11/ex11/ft_list.h
@@ -0,0 +1,51 @@
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct s_list {
+    void   *data;
+    struct s_list *next;
+} t_list;
+
+#include <stdlib.h>
+
+t_list  *ft_create_elem(void *data)
+{
+    t_list  *list_element;
+    
+    list_element = malloc(sizeof(t_list));
+    if (!list_element)
+        return  (NULL);
+    
+    list_element->data = malloc(sizeof(void *));
+    if (list_element->data == NULL)
+        return (NULL);
+    list_element->next = malloc(sizeof(t_list *));
+    if (list_element->next == NULL)
+        return (NULL);
+   
+    list_element->data = data;
+    list_element->next = NULL;
+    return (list_element);
+}
+
+#include <stdio.h>
+
+void  ft_list_push_front(t_list **begin_list, void *data)
+{
+    t_list  *list_element;
+    
+    list_element = ft_create_elem(data);
+    list_element->next = *begin_list;
+    *begin_list = list_element;
+}
+
+t_list *ft_list_last(t_list *begin_list)
+{
+    
+    while (begin_list->next)
+        begin_list = begin_list->next;
+    return (begin_list);
+}
+
+#endif
